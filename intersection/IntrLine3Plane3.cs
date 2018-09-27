7aeae467b89563d33a278c691e4642afb27d6974
Bery0za <bereznevtree@yandex.ru>
2018-09-27 23:18:05 +0500

    Added Line3-Plane3 intersection

---
+++ intersection/IntrLine3Plane3.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+
+namespace g3
+{
+    // ported from WildMagic5 IntrPlane3Plane3
+    // use Test() for fast boolean query, does not compute intersection info
+    // use Find() to compute full information
+    public class IntrLine3Plane3
+    {
+        Line3d line;
+        public Line3d Line
+        {
+            get { return line; }
+            set { line = value; Result = IntersectionResult.NotComputed; }
+        }
+
+        Plane3d plane;
+        public Plane3d Plane
+        {
+            get { return plane; }
+            set { plane = value; Result = IntersectionResult.NotComputed; }
+        }
+        
+        // result flags
+		public int Quantity = 0;
+		public IntersectionResult Result = IntersectionResult.NotComputed;
+		public IntersectionType Type = IntersectionType.Empty;
+        
+        // only valid if intersection type is Point
+        public Vector3d Point;
+        
+        // only valid if intersection type is Line
+        public Line3d CoincidentLine;
+
+		public IntrLine3Plane3(Line3d l, Plane3d p)
+		{
+		    line = l;
+		    plane = p;
+		}
+
+        public IntrLine3Plane3 Compute()
+        {
+            Find();
+            return this;
+        }
+
+        public bool Find()
+        {
+            double DdN = line.Direction.Dot(plane.Normal);
+            double signedDistance = plane.DistanceTo(line.Origin);
+            
+            if (Math.Abs(DdN) > MathUtil.ZeroTolerance)
+            {
+                // The line is not parallel to the plane, so they must intersect.
+                Quantity = 1;
+                Point = Line.PointAt(-signedDistance / DdN);
+                Type = IntersectionType.Point;
+                return true;
+            }
+
+            // The line and plane are parallel. Determine if they are numerically
+            // close enough to be coincident.
+            if (Math.Abs(signedDistance) <= MathUtil.ZeroTolerance)
+            {
+                // The line is coincident with the plane, so choose t = 0 for the
+                // parameter.
+                Quantity = 1;
+                Point = Line.PointAt(0);
+                CoincidentLine = new Line3d(line.Origin, line.Direction);
+                Type = IntersectionType.Line;
+                return true;
+            }
+
+            Type = IntersectionType.Empty;
+            return false;
+        }
+
+        public bool Test()
+        {
+            double DdN = line.Direction.Dot(plane.Normal);
+            if (Math.Abs(DdN) > MathUtil.ZeroTolerance)
+            {
+                // The line is not parallel to the plane, so they must intersect.
+                // The line parameter is *not* set, since this is a test-intersection
+                // query.
+                Quantity = 1;
+                Type = IntersectionType.Point;
+                return true;
+            }
+
+            // The line and plane are parallel. Determine if they are numerically
+            // close enough to be coincident.
+            double signedDistance = plane.DistanceTo(line.Origin);
+            if (Math.Abs(signedDistance) <= MathUtil.ZeroTolerance)
+            {
+                Quantity = 1;
+                Type = IntersectionType.Line;
+                return true;
+            }
+
+            Type = IntersectionType.Empty;
+            return false;
+        }
+    }
+}
